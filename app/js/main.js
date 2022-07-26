dac6558644cd473dc68df2ffaabeaf66ab3aa24b
LasttoSay <lasttosaytoyou@gmail.com>
2022-07-26 07:28:15 +0200

    added header adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
-
+  $('.menu__responsive').on('click',  function(){
+    $('.menu').toggleClass('active');
+  });
+  
+  $(".menu a").on("click", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+ });
 });
